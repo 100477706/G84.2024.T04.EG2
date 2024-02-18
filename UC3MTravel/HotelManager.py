00d0734ecbf06b774890f3d8a65614201b0f310a
Joaquín Pujol Carrillo <100495826@gmail.com>
2024-02-18 21:16:44 +0100

    funcion validate_credit_card

--- UC3MTravel/HotelManager.py
+++ UC3MTravel/HotelManager.py
@@ -1,38 +1,69 @@
 import json
-from .HotelManagementException import HOTEL_MANAGEMENT_EXCEPTION
-from .HotelReservation import HOTEL_RESERVATION
+from .HotelManagementException import HotelManagementException
+from .HotelReservation import HotelReservation
 
 
-class HOTEL_MANAGER:
+class HotelManager:
     def __init__(self):
         pass
 
-    def ValidateCreditCard(self, tarjeta):
+    def validatecreditcard(self, x):
         # PLEASE INCLUDE HERE THE CODE FOR VALIDATING THE GUID
         # RETURN TRUE IF THE GUID IS RIGHT, OR FALSE IN OTHER CASE
-        return True
+        # Comprobamos si la tarjeta tiene 16 dígitos
+        if len(str(x)) != 16:
+            return False
 
-    def ReaddatafromJSOn(self, fi):
+        # Convertimos el número a lista y obtenemos el supuesto valor del último dígito según Luhn
+        num_lista = []
+        for a in range(len(str(x))-1):
+            num_lista.append(str(x)[a])
+
+        # Guardamos la suma del algoritmo de Luhn en la variable suma
+        suma = 0
+        # Cada dígito par lo duplicamos y sumamos cada dígito del resultado a suma
+        # Cada dígito impar lo sumamos directamente a suma
+        for a in range(len(num_lista)):
+            if a % 2 != 0:
+                suma += int(num_lista[a])
+            else:
+                num_actual = int(num_lista[a])
+                num_actual *= 2
+                if num_actual >= 10:
+                    suma += int(str(num_actual)[0]) + int(str(num_actual)[1])
+                else:
+                    suma += num_actual
+        # Calculamos el resultado del último dígito
+        ultimo_digito = (suma * 9) % 10
+        # Comprobamos si el último dígito es el que debería ser según Luhn
+        if str(ultimo_digito) == str(x)[len(str(x)) - 1]:
+            return True
+        else:
+            return False
+
+
+
+
+
+    def ReaddatafromJSOn( self, fi):
 
         try:
-            with open(fi, encoding="utf-8") as variable_f:
-                data = json.load(variable_f)
-        except FileNotFoundError as variable_e:
-            raise HOTEL_MANAGEMENT_EXCEPTION("Wrong file or file path") from variable_e
-        except json.JSONDecodeError as variable_e:
-            raise HOTEL_MANAGEMENT_EXCEPTION("JSON Decode Error - Wrong JSON Format")\
-                from variable_e
+            with open(fi) as f:
+                DATA = json.load(f)
+        except FileNotFoundError as e:
+            raise HotelManagementException("Wrong file or file path") from e
+        except json.JSONDecodeError as e:
+            raise HotelManagementException("JSON Decode Error - Wrong JSON Format") from e
+
 
         try:
-            variable_c = data["CreditCard"]
-            variable_p = data["phoneNumber"]
-            req = HOTEL_RESERVATION(id_card="12345678Z", credit_card_numb=variable_c,
-                                    name_and_surname="John Doe", phone_number=variable_p,
-                                    room_type="single", num_days=3)
-        except KeyError as variable_e:
-            raise HOTEL_MANAGEMENT_EXCEPTION("JSON Decode Error - Invalid JSON Key") from variable_e
-        if not self.ValidateCreditCard(variable_c):
-            raise HOTEL_MANAGEMENT_EXCEPTION("Invalid credit card number")
+            c = DATA["CreditCard"]
+            p = DATA["phoneNumber"]
+            req = HotelReservation(IDCARD="12345678Z",creditcardNumb=c,nAMeAndSURNAME="John Doe",phonenumber=p,room_type="single",numdays=3)
+        except KeyError as e:
+            raise HotelManagementException("JSON Decode Error - Invalid JSON Key") from e
+        if not self.validatecreditcard(c):
+            raise HotelManagementException("Invalid credit card number")
 
         # Close the file
         return req
